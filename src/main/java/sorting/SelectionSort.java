821e38c0dd51e7b4e7fc2afb6ca8f3de79fc5bf0
Romil Bhawsar <romilbhawsar20@gmail.com>
2026-09-11 19:54:49 +0530

    Implemented Selection Sort Technique

---
+++ src/main/java/sorting/SelectionSort.java
@@ -0,0 +1,26 @@
+//Selection Sort → finds minimum and swaps once per pass
+package sorting;
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        int n = arr.length;
+        for(int i = 0;i<=n-2;i++){
+            int minIndex = i;
+            for(int j = i+1;j<=n-1;j++){
+                if(arr[j]<arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+            //swap
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+
+        }
+        // Print the sorted array
+        for(int i = 0;i<n;i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
