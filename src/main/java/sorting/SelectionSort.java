9737b9b96690fb1008745a3434af507d17c4ef65
Romil Bhawsar <romilbhawsar20@gmail.com>
2026-09-12 13:23:05 +0530

    Implemented Selection Sort Technique

--- src/main/java/sorting/SelectionSort.java
+++ src/main/java/sorting/SelectionSort.java
@@ -4,9 +4,9 @@ public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {64, 25, 12, 22, 11};
         int n = arr.length;
-        for(int i = 0;i<=n-2;i++){
+        for(int i = 0;i<n-1;i++){
             int minIndex = i;
-            for(int j = i+1;j<=n-1;j++){
+            for(int j = i+1;j<n;j++){
                 if(arr[j]<arr[minIndex]){
                     minIndex = j;
                 }
