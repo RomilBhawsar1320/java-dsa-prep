81923d6dd87f8f1926634c201774f145f2a46184
Romil Bhawsar <romilbhawsar20@gmail.com>
2026-09-15 20:48:22 +0530

    Learning Linked List Data Structure

--- src/main/java/linkedlist/LinkedList.java
+++ src/main/java/linkedlist/LinkedList.java
@@ -21,4 +21,31 @@ public class LinkedList {
         size = 1;
     }
 
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getSize() {
+        System.out.println("Size: " + size);
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(10);
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getSize();
+        myLinkedList.printList();
+    }
+
 }
