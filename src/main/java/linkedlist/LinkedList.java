a3ef681f2bffd1f5fa199bea8757e3ccba423a15
Romil Bhawsar <romilbhawsar20@gmail.com>
2026-09-22 16:30:30 +0530

    Remove Duplicates method added

--- src/main/java/linkedlist/LinkedList.java
+++ src/main/java/linkedlist/LinkedList.java
@@ -197,7 +197,31 @@ public class LinkedList {
         }
         return slow;
     }
-
+    public void removeDuplicates() {
+        if (head == null) return;
+        Node current = head;
+        while (current != null && current.next != null) {
+            if (current.value == current.next.value) {
+                current.next = current.next.next;
+                size--;
+            } else {
+                current = current.next;
+            }
+        }
+        tail = current; // Update tail to the last unique node/
+    }
+    /*
+    REMOVE DUPLICATES EXPLANATION:
+1. Check if the head is null. If it is, return immediately as there are no duplicates to remove.
+2. Initialize a pointer current to the head of the list.
+3. Traverse the list using a while loop that continues as long as current and current.next are not null.
+4. Inside the loop, compare the value of the current node with the value of the next node (current.next).
+5. If the values are equal, it means there is a duplicate. In this case, update the next pointer of the current node to skip the next node (
+current.next = current.next.next), effectively removing the duplicate node from the list. Decrement the size of the list by 1.
+6. If the values are not equal, move the current pointer to the next node (current = current.next).
+7. After the loop, update the tail pointer to point to the last unique node in the list (tail = current). This ensures that the tail correctly reflects the end of the
+    list after duplicates have been removed.
+     */
 
     public static void main(String[] args) {
         LinkedList myLinkedList = new LinkedList(10);
@@ -273,6 +297,7 @@ public Node findKthFromEnd(int k) {
 }
 
          */
+
 
 
     }
